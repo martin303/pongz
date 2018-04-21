d56a0bd62440e1b5bd96d9c5369304c986c5fced
tompong <tomkare@kth.se>
2018-04-21 19:09:31 +0200

    player score imp

---
+++ Pongz/Assets/PlayerOneController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneController : MonoBehaviour {
+    public float speed = 0F;
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        translation *= Time.deltaTime;
+        transform.Translate(0, translation, 0);
+    }
+}
--- Pongz/Assets/Scripts/Bouncy_ball.cs
+++ Pongz/Assets/Scripts/Bouncy_ball.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bouncy_ball : MonoBehaviour
 {
@@ -29,45 +31,81 @@ public class Bouncy_ball : MonoBehaviour
     //    }
     //}
 
+    public Text textScore;
+    public Text textScore2;
+    public int playerOneScore;
+    public int playerTwoScore;
+
     float thrust = 500;
     private Rigidbody2D rb;
-    private int playerOneScore;
+
 
     void Start()
     {
-
+        UpdatePlayerOneScore();
         rb = GetComponent<Rigidbody2D>();
         rb.AddForce(transform.up * thrust);
         rb.AddForce(transform.right * thrust);
-       // Debug.Log("StartHAHAHAA");
-
+        // Debug.Log("StartHAHAHAA");
+        playerOneScore = 0;
 
     }
-
+    
     void FixedUpdate()
     {
-
     }
-
+    void Update()
+    {
+        //check if x speed is to low and double
+       
+        if (rb.velocity.x <= 4 && rb.velocity.x >= -4)
+        {
+            Debug.Log("OLD x speed:" + rb.velocity.x);
+            rb.velocity = new Vector2(rb.velocity.x*2, rb.velocity.y);
+            Debug.Log("NEW x Speed: " + rb.velocity.x);
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("OnTriggerEnter");
+
     }
 
 
     void OnCollisionEnter2D(Collision2D col)
     {
-    //    if (col.transform.position.x >= 0 && col.transform.posit) {
-        if (transform.position.x > 10) {
-            Debug.Log("X: 12 hit");
+        //Change to check if collision occurs with edge on right/left side.
+        if (transform.position.x > 7) {
+      //      Debug.Log("X: 12 hit");
             playerOneScore++;
+            UpdatePlayerOneScore();
+            Debug.Log(playerOneScore);
         }
 
-    //    Debug.Log("OnCollish");
+        if (transform.position.x < -7)
+        {
+            //      Debug.Log("X: 12 hit");
+            playerTwoScore++;
+            UpdatePlayerTwoScore();
+            Debug.Log(playerTwoScore);
+        }
+
+        //    Debug.Log("OnCollish");
         if (col.gameObject.name == "OuterSquare")
         {
      //       Debug.Log("OuterSwwuar");
         }
+    }
+
+    private void UpdatePlayerTwoScore()
+    {
+
+        textScore2.text = "Score: " + playerTwoScore.ToString();
+    }
+
+    void UpdatePlayerOneScore()
+    {
+        textScore.text = "Score: " + playerOneScore.ToString();
     }
 }
 
