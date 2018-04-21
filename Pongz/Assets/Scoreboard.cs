d5426c4f212181e05b1d6288669a7efc4acba15f
tompong <tomkare@kth.se>
2018-04-21 21:24:11 +0200

    Networking and ScoreBooard

---
+++ Pongz/Assets/Scoreboard.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour {
+
+    
+    public Collider2D ball;
+    public Collider2D leftSide;
+    public Collider2D rigtSide;
+    int playerOneScore;
+    int playerTwoScore;
+    private void Start()
+    {
+        playerOneScore = 0;
+        playerTwoScore = 0;
+    }
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+
+
+        //Change to check if collision occurs with edge on right/left side.
+        //if (transform.position.x > 7)
+        //{
+        //    //      Debug.Log("X: 12 hit");
+        //    playerOneScore++;
+        //    UpdatePlayerScore(playerOneScore);
+        //    Debug.Log(playerOneScore);
+        //}
+
+        //if (transform.position.x < -7)
+        //{
+        //    //      Debug.Log("X: 12 hit");
+        //    playerTwoScore++;
+        //    UpdatePlayerScore(playerTwoScore);
+        //    Debug.Log(playerTwoScore);
+        //}
+    }
+
+}
--- Pongz/Assets/Scripts/Bouncy_ball.cs
+++ Pongz/Assets/Scripts/Bouncy_ball.cs
@@ -6,54 +6,24 @@ using UnityEngine.UI;
 
 public class Bouncy_ball : MonoBehaviour
 {
-    //float horizontal;
-    //float vertical;
-    //float speed;
-
-    //private void start()
-    //{
-    //    speed = 1;
-    //}
-
-
-    //update is called once per frame
-    //void fixedupdate()
-    //{
-    //    horizontal = input.getaxis("horizontal");
-    //    vertical = input.getaxis("vertical");
-
-
-    //    check should cause sliding.
-    //    if (horizontal != 0 || vertical != 0)
-    //    {
-    //        vector2 movement = new vector2(horizontal, vertical) * speed;
-    //        getcomponent<transform>().position = new vector3(movement.x, movement.y, transform.position.z);
-    //    }
-    //}
-
     public Text textScore;
     public Text textScore2;
-    public int playerOneScore;
-    public int playerTwoScore;
 
+
+    AudioSource audio;
     float thrust = 500;
     private Rigidbody2D rb;
 
 
     void Start()
     {
-        UpdatePlayerOneScore();
         rb = GetComponent<Rigidbody2D>();
         rb.AddForce(transform.up * thrust);
         rb.AddForce(transform.right * thrust);
-        // Debug.Log("StartHAHAHAA");
-        playerOneScore = 0;
-
+        audio = GetComponent<AudioSource>();
     }
     
-    void FixedUpdate()
-    {
-    }
+  
     void Update()
     {
         //check if x speed is to low and double
@@ -65,47 +35,14 @@ public class Bouncy_ball : MonoBehaviour
             Debug.Log("NEW x Speed: " + rb.velocity.x);
         }
     }
-    private void OnTriggerEnter(Collider other)
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("OnTriggerEnter");
-
-    }
-
-
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        //Change to check if collision occurs with edge on right/left side.
-        if (transform.position.x > 7) {
-      //      Debug.Log("X: 12 hit");
-            playerOneScore++;
-            UpdatePlayerOneScore();
-            Debug.Log(playerOneScore);
-        }
-
-        if (transform.position.x < -7)
+        if (collision.gameObject.name == "PlayerOne" || collision.gameObject.name == "PlayerTwo")
         {
-            //      Debug.Log("X: 12 hit");
-            playerTwoScore++;
-            UpdatePlayerTwoScore();
-            Debug.Log(playerTwoScore);
-        }
-
-        //    Debug.Log("OnCollish");
-        if (col.gameObject.name == "OuterSquare")
-        {
-     //       Debug.Log("OuterSwwuar");
+            audio.Play();
         }
     }
 
-    private void UpdatePlayerTwoScore()
-    {
-
-        textScore2.text = "Score: " + playerTwoScore.ToString();
-    }
-
-    void UpdatePlayerOneScore()
-    {
-        textScore.text = "Score: " + playerOneScore.ToString();
-    }
 }
 
