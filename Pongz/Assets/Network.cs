ef80c0a9331becb16078a316a52733dae2847f40
tompong <tomkare@kth.se>
2018-04-21 22:24:32 +0200

    Network update

---
+++ Pongz/Assets/Network.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+
+public class Network : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        NetworkManager manager = GetComponent<NetworkManager>();
+        manager.StartServer();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
