a22d350734a477ee3a7e57e61dafcf1ad3a977e7
Carl Uddman <cuddman@kth.se>
2018-04-22 14:18:51 +0200

    Fixed playercontrolls for networking

--- Pongz/Assets/PlayerOneController.cs
+++ Pongz/Assets/PlayerOneController.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerOneController : MonoBehaviour {
+public class PlayerOneController : NetworkBehaviour {
     public float speed = 0F;
     void Update()
     {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
         float translation = Input.GetAxis("Vertical") * speed;
         translation *= Time.deltaTime;
         transform.Translate(0, translation, 0);
--- Pongz/Assets/script.cs
+++ Pongz/Assets/script.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class script : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * 0.1f;
+        var z = Input.GetAxis("Vertical") * 0.1f;
+
+        transform.Translate(x, 0, z);
+    }
 }
