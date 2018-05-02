c8df2a6b05b89667460a5957a7745e1cea1a0701
cuddman <cuddman@kth.se>
2018-05-02 21:54:23 +0200

    NEW Hexagon3PlayerMap

---
+++ Pongz/Assets/ThreePlayerMap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreePlayerMap : MonoBehaviour {
+
+    // Use this for initialization
+    void Start ()
+    {
+        List<Vector2> hexagonPoints = new List<Vector2>();
+        EdgeCollider2D map = GetComponent<EdgeCollider2D>();
+        float cameraHeight = Camera.main.orthographicSize *2;
+        float s = cameraHeight / 2;
+        float r = Mathf.Sqrt(3)/2*s;
+
+        hexagonPoints.Add(new Vector2(0, -s));
+        hexagonPoints.Add(new Vector2(r, -s / 2));
+        hexagonPoints.Add(new Vector2(r, s / 2));
+        hexagonPoints.Add(new Vector2(0, s));
+        hexagonPoints.Add(new Vector2(-r, s / 2));
+        hexagonPoints.Add(new Vector2(-r, -s / 2));
+        hexagonPoints.Add(new Vector2(0, -s));
+        map.points = hexagonPoints.ToArray();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
